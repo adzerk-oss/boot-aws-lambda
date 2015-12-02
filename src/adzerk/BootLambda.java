d5588de73f93d06d4c0f363360ab4f8f432ddd9c
Alan Dipert <alan@dipert.org>
2015-12-02 01:55:08 -0500

    ws

--- src/adzerk/BootLambda.java
+++ src/adzerk/BootLambda.java
@@ -15,35 +15,35 @@ public class BootLambda implements RequestStreamHandler {
 
   public String PROPERTY_FILE_NAME = "/adzerk.lambda.properties";
   public String HANDLER_PROP_NAME = "handler-var";
-  
+
   public IFn require   = Clojure.var("clojure.core", "require");
   public IFn namespace = Clojure.var("clojure.core", "namespace");
   public IFn name      = Clojure.var("clojure.core", "name");
-  
+
   public Symbol sym;
   public String symNs;
   public String symName;
-  
+
   public Properties props = new Properties();
-  
+
   public IFn handler;
-  
+
   public BootLambda() throws Exception {
     try (final InputStream stream = BootLambda.class.getResourceAsStream(PROPERTY_FILE_NAME)) {
       props.load(stream);
-	} catch (Exception e) {
+    } catch (Exception e) {
       System.err.println("Error loading "+PROPERTY_FILE_NAME);
-	  throw e;
-	}
+      throw e;
+    }
     sym     = (Symbol)Clojure.read(props.getProperty(HANDLER_PROP_NAME));
     symNs   = (String)namespace.invoke(sym);
     symName = (String)name.invoke(sym);
     require.invoke(Clojure.read(symNs));
-    handler = Clojure.var(symNs, symName); 
+    handler = Clojure.var(symNs, symName);
   }
-  
+
   public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
-	System.out.println("Invoking "+sym.toString());
+    System.out.println("Invoking "+sym.toString());
     handler.invoke(input, output, context);
   }
 }
